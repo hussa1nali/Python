11178132e5850a60994c9e19fd4bf34040f3ae34
hussa1nali <huchein@gmail.com>
2023-04-09 08:10:37 +0800

    Cleaned up columns and created pivot/chart

--- Real-estateData_API.py
+++ Real-estateData_API.py
@@ -6,8 +6,14 @@ from pandas import json_normalize  # Import json_normalize from the correct modu
 
 url = "https://realty-in-au.p.rapidapi.com/properties/list"
 
-querystring = {"channel":"buy","searchLocation":"Willetton, WA 6155","searchLocationSubtext":"Region","type":"region","page":"1","pageSize":"30","sortType":"relevance","surroundingSuburbs":"true","ex-under-contract":"false"}
-
+querystring = {"channel":"buy",
+               "searchLocation":"Willetton, WA 6155",
+               "searchLocationSubtext":"Region",
+               "type":"region","page":"1",
+               "pageSize":"30",
+               "sortType":"relevance",
+               "surroundingSuburbs":"true",
+               "ex-under-contract":"true"}
 headers = {
     "X-RapidAPI-Key": "db5ad576ddmsh6a06fb52f495a97p161174jsnd62ba1e007f8",
     "X-RapidAPI-Host": "realty-in-au.p.rapidapi.com"
@@ -93,11 +99,11 @@ columns_to_drop = ['advertising_region',
                    'agency_logo_links_default',
                    'agency_logo_links_large',
                    'agency_agencyId', 
-                   'agency_email',
+                   #'agency_email',
                    'mainImage_server',
                    'mainImage_name', 
                    'mainImage_uri',
-                   'modifiedDate_value'
+                   'modifiedDate_value',
                    'generalFeatures_bedrooms_type',
                    'generalFeatures_bedrooms_value',
                    'generalFeatures_bathrooms_label', 
@@ -108,8 +114,133 @@ columns_to_drop = ['advertising_region',
                    'generalFeatures_parkingSpaces_value',
                    'landSize_displayAppAbbreviated',
                    'landSize_displayApp', 
-                   'landSize_unit'
+                   'landSize_unit',
+                   'landSize_display',
+                   'propertyFeatures',
+                   'status_label',
+                   'description',
+                   'title',
+                   'nextInspectionTime_endTimeDisplay',
+                   'nextInspectionTime_startTime',
+                   'nextInspectionTime_endTime',
+                   'nextInspectionTime_auction',
+                   'address_locality',
+                   'address_postcode',
+                   'builderProfile_hasDesignsOnPage',
+                   'agencyListingId',
+                   'generalFeatures_bedrooms_label'
+
+
                   ]
 
 # Drop columns from DataFrame
 newdf = df.drop(columns_to_drop, axis=1)
+# Rename columns
+# create a dictionary to map the old column names to new ones
+new_names = {
+    'features_general_bedrooms': 'Bedrooms',
+    'features_general_bathrooms': 'Bathrooms',
+    'features_general_parkingSpaces': 'Garage',
+    'landSize_value': 'Land_size',
+    'price_display': 'Price',
+    'address_streetAddress': 'Address',
+    'address_suburb': 'Suburb',
+    'address_postCode': 'Post_code',
+    'status_type': 'Status',
+    'nextInspectionTime_dateDisplay': 'Next_Inspection_Date',
+    'nextInspectionTime_startTimeDisplay': 'Next_Inspection_Time'
+}
+# rename the columns using the dictionary
+newdf = newdf.rename(columns=new_names)
+
+## -------------this code below is to fix the sale price
+import re
+
+
+# define a regular expression pattern to match numbers
+pattern = r'\d{1,3}(?:,\d{3})*\.?\d*'
+
+# define a function to extract the number from the text
+# define a function to extract the number from the text
+def extract_number(text):
+    # check if text contains "million"
+    if 'million' in text.lower():
+        # find the first number between the dollar sign and "million"
+        match = re.search(r'\$(%s)\s*million' % pattern, text, re.IGNORECASE)
+        if match:
+            # extract the matched string
+            number_str = match.group(1)
+            # remove commas and convert to float
+            number = float(number_str.replace(',', ''))
+            # multiply by 1,000,000 and round to nearest integer
+            number = round(number * 1000000)
+        else:
+            number = None
+    # check if text contains "k" or "ks"
+    elif 'k' in text.lower():
+        # check if text starts with "k" or "ks"
+        if text.lower().startswith(('k', 'ks')):
+            # assume the number is 1 and multiply by 1000
+            number = 1000
+        else:
+            # remove any non-digit characters and convert to integer
+            number_str = re.sub(r'[^\d]', '', text)
+            if number_str.strip():  # check if number_str is not empty or only whitespace
+                if len(number_str) <= 3:
+                    number = int(number_str) * 1000
+                else:
+                    number = int(number_str)
+            else:
+                number = None
+    else:
+        # find the first number in the text using the pattern
+        match = re.search(pattern, text)
+        if match:
+            # extract the matched string
+            number_str = match.group(0)
+            # remove commas and convert to float
+            if ',' in number_str:
+                number_str = number_str.replace(',', '')
+            number = float(number_str)
+            # round to nearest integer
+            number = round(number)
+        else:
+            number = None
+    return number
+
+
+
+
+# apply the function to the Price column to create a new column with the extracted numbers
+newdf['Price_Num'] = newdf['Price'].apply(extract_number)
+
+# print the output column to the console
+#print(newdf['Price_Num'])
+
+
+#the colde below is to analyse prices
+import matplotlib.pyplot as plt
+import seaborn as sns
+# replace missing or invalid values in the Land_size column with zero
+newdf['Land_size'] = pd.to_numeric(newdf['Land_size'], errors='coerce').fillna(0)
+
+# create a pivot table with the median price for each combination of propertyType, Bedrooms, and Land_size
+pivot_table = newdf.pivot_table(values='Price_Num', index=['propertyType', 'Bedrooms'], columns='Land_size', aggfunc='median')
+# Generate a color palette with 5 colors for the 5 land size levels
+palette = sns.color_palette("coolwarm", n_colors=5)
+
+# sort the pivot table by the 'Bedrooms' column in descending order
+pivot_table = pivot_table.sort_values(by='Bedrooms', ascending=False)
+
+# Create the heatmap with the color palette
+sns.heatmap(pivot_table, annot=False, fmt='.0f', cmap=palette, cbar_kws={'label': '{Price} (m)'})
+
+# set the title of the plot
+plt.title('Median Price by House Type, Bedrooms, and Land Size')
+
+# show the plot
+plt.show()
+
+newdf.to_csv('Data.csv')
+pivot_table.to_csv('Pivot.csv')
+
